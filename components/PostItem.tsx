e0ce85db4c9885c044a6f6de77c6ae85e402b370
komosyu4649 <komosyu4649@gmail.com>
2022-09-14 09:38:53 +0900

    wip: tailwind.css

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { postType } from '../type';
 
 const PostItem = ({ post }: { post: postType }) => {
-  return <li key={post.id}>{post.title}</li>;
+  return (
+    <li key={post.id} className="text-red-700">
+      {post.title}
+    </li>
+  );
 };
 
 export default PostItem;
